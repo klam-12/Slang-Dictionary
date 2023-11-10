5c2a348834b839194d0a672411288231a3cd511a
KhanhLam <90090516+klam-12@users.noreply.github.com>
2023-11-10 18:50:32 +0700

    Export Jar file. Finish project

---
+++ src/main/java/tester/SlangApp.java
@@ -0,0 +1,13 @@
+package tester;
+
+import view.dictionaryView;
+
+/**
+ * @author Khanh Lam
+ */
+public class SlangApp {
+    // Run program here
+    public static void main(String[] args) {
+        dictionaryView dictView = new dictionaryView();
+    }
+}
